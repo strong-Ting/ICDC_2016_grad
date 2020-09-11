68e9539a57058ae426ff5c698b4d5dbf38029f66
strong-Ting <s8527502987@gmail.com>
2020-09-11 14:48:12 +0800

    -cycle modify to 10 ns
    time: 1,587,670 ns
    area: 5643
    
    score = time*area = 8,959,221,810
    
    score < 12,000,000,000
    
    A

--- INCA_libs/irun.lnx86.11.10.nc/files.ts
+++ INCA_libs/irun.lnx86.11.10.nc/files.ts
@@ -1,3 +1,3 @@
-1599411555 /home/M10912039/GitHub/CIC_2016_grad/testfixture.v
-1599374076 /home/M10912039/GitHub/CIC_2016_grad/LBP.v
-1599409899 /home/M10912039/GitHub/CIC_2016_grad/LBP_syn.v
+1599806748 /home/M10912039/GitHub/CIC_2016_grad/testfixture.v
+1599806713 /home/M10912039/GitHub/CIC_2016_grad/LBP.v
+1599806713 /home/M10912039/GitHub/CIC_2016_grad/LBP_syn.v
