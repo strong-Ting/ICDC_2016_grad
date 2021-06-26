03a1fa504de34bda9dd1c13d3407123a8dec099f
strong-ting <s8527502987@gmail.com>
2021-06-26 22:27:57 +0800

    -del sim dir

---
+++ finial/syn/DC_syn.tcl
@@ -0,0 +1,21 @@
+#Read All Files
+read_verilog ./src/CLE.v
+current_design CLE
+link
+
+#Setting Clock Constraints
+source -echo -verbose CLE_DC.sdc
+
+#Synthesis all design
+compile -map_effort high -area_effort high
+compile -map_effort high -area_effort high -inc
+
+
+
+write -format ddc     -hierarchy -output "CLE_syn.ddc"
+write_sdf CLE_syn.sdf
+write_file -format verilog -hierarchy -output CLE_syn.v
+report_area > area.log
+report_timing > timing.log
+report_qor   >  CLE_syn.qor
+
